6b634c6ae6914be9697a4965ac24910effa77759
rachidi <96305570+Joe-0009@users.noreply.github.com>
2024-06-13 19:35:15 +0000

    gg

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    const amenities = {};
+
+    $('input[type="checkbox"]').change(function () {
+        if ($(this).is(':checked')) {
+            amenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenities[$(this).data('id')];
+        }
+        const amenitiesList = Object.values(amenities).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+});
